fa7f609dbbd3abb93ee00129d8368c18f62880b5
Fernando Reyes <fersland@outlook.es>
2023-11-02 15:09:18 -0500

    Funcion eliminar funcionando correctamente.

--- Consultorio_Seguros_Two/Controllers/ClientesController.cs
+++ Consultorio_Seguros_Two/Controllers/ClientesController.cs
@@ -2,6 +2,7 @@
 using Consultorio_Seguros.Repositories;
 using Dapper;
 using Microsoft.AspNetCore.Mvc;
+using System.Data;
 
 namespace Consultorio_Seguros.Web.Controllers
 {
@@ -36,13 +37,60 @@ namespace Consultorio_Seguros.Web.Controllers
                 parameters.Add("@Nombre", cliente.Nombre);
                 parameters.Add("@Telefono", cliente.Telefono);
                 parameters.Add("@Edad", cliente.Edad);
-                _clienteRepo.DMLCliente("InsertCliente", parameters, System.Data.CommandType.StoredProcedure);
+                _clienteRepo.DMLCliente("InsertCliente", parameters, CommandType.StoredProcedure);
                 return RedirectToAction("Index");
             }
             return View(cliente);
         }
 
+        [HttpGet]
+        public IActionResult Edit(int id)
+        {
+            DynamicParameters parameters = new DynamicParameters();
+            parameters.Add("@Id", id);
+            var cliente = _clienteRepo.GetClienteById("GetClienteById", parameters, CommandType.StoredProcedure);
+            return View(cliente);
+        }
 
+        [HttpPost]
+        public IActionResult Edit(Cliente cliente)
+        {
+            if(ModelState.IsValid)
+            {
+                DynamicParameters parameters = new DynamicParameters();
+                parameters.Add("@Id", cliente.Id);
+                parameters.Add("@Cedula", cliente.Cedula);
+                parameters.Add("@Nombre", cliente.Nombre);
+                parameters.Add("@Telefono", cliente.Telefono);
+                parameters.Add("@Edad", cliente.Edad);
+                _clienteRepo.DMLCliente("UpdateCliente", parameters, CommandType.StoredProcedure);
+                return RedirectToAction("Index");
+            }
+            return View(cliente);
+        }
+
+        [HttpGet]
+        public IActionResult Delete(int id)
+        {
+            DynamicParameters parameters = new DynamicParameters();
+            parameters.Add("@Id", id);
+            var cliente = _clienteRepo.GetClienteById("GetClienteById", parameters, CommandType.StoredProcedure);
+            return View(cliente);
+        }
+
+        [HttpPost]
+        public IActionResult Delete(int id, Cliente cliente)
+        {
+            if (ModelState.IsValid)
+            {
+                DynamicParameters parameters = new DynamicParameters();
+                parameters.Add("@Id", id);
+                _clienteRepo.DMLCliente("DeleteCliente", parameters, CommandType.StoredProcedure);
+                TempData["successMessage"] = "Dato eliminado correctamente.";
+                return RedirectToAction("Index");
+            }
+            return View(cliente);
+        }
 
     }
 }
